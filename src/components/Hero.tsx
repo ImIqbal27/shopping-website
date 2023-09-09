c6ac05aa7ab19337213384be58023539e5780882
Iqbal <iamhasan9501@gmail.com>
2023-09-09 14:44:51 +0600

    hero section done

---
+++ src/components/Hero.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React from "react";
+import Slider from "react-slick";
+import Slide from "./Slide";
+
+const Hero = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  const slideDatas = [
+    {
+      id: 0,
+      img: "/banner-1.jpg",
+      title: "Trending Item",
+      mainTitle: "WOMEN'S LATEST FASHION SALE",
+      price: "$20",
+    },
+    {
+      id: 1,
+      img: "/banner-2.jpg",
+      title: "Trending Accessories",
+      mainTitle: "MODERN SUNGLASSES",
+      price: "$15",
+    },
+    {
+      id: 2,
+      img: "/banner-3.jpg",
+      title: "Sale Offer",
+      mainTitle: "NEW FASHION SUMMER SALE",
+      price: "$30",
+    },
+  ];
+  return (
+    <div>
+      <div className="container pt-6 lg:pt-0">
+        <Slider {...settings}>
+          {slideDatas.map((slide) => (
+            <Slide
+              key={slide.id}
+              img={slide.img}
+              title={slide.title}
+              mainTitle={slide.mainTitle}
+              price={slide.price}
+            />
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
